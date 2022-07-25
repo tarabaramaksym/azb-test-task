268ee79da153f7d4ac134f452ed1e75ae1d05ace
tarabaramaksym <tarabara.maksym@protonmail.com>
2022-07-25 13:20:04 +0300

    Added validation

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -10,14 +10,60 @@ import { getUsers } from "../../Redux/actions";
 const SignUpForm = props => {
 
   const [positions, setPositions] = useState();
-  const [isValid, setIsValid] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
+
   const [form, setForm] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    position_id: 1,
-    photo: ''
-  });
+    name:
+    {
+      type: 'text',
+      value: '',
+      placeholder: 'Your name',
+      validate: (field) => field.length > 2 && field.length <= 60,
+      isValid: false,
+      triedInput: false,
+      errorMessage: 'Name needs to be longer than 2 and shorter than 60 letters',
+      inFocus: false
+    },
+    email: {
+      type: 'email',
+      value: '',
+      placeholder: 'Email',
+      validate: (field) => /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(field),
+      isValid: false,
+      triedInput: false,
+      errorMessage: 'Not valid email format',
+      inFocus: false
+    },
+
+    phone: {
+      type: 'text',
+      value: '',
+      placeholder: 'Phone',
+      validate: (field) => (field.indexOf('+380') == 0 || field.indexOf('380') == 0 || field.indexOf('+38 (0') == 0) && (field.length === 12 || field.length === 13),
+      isValid: false,
+      triedInput: false,
+      errorMessage: 'Phone needs to start with the +380 code and have 12 digits',
+      preSend: (value) => value[0] === '+' ? value : `+${value}`,
+      inFocus: false
+    },
+    position_id: {
+      value: 1,
+      render: false,
+      validate: () => true,
+      isValid: true,
+      triedInput: true
+    },
+    photo: {
+      value: '',
+      validate: (field) => field.size <= (5 * 1000000),
+      isValid: false,
+      triedInput: false,
+      errorMessage: 'Photo needs to be smaller than 5 MB',
+      render: false,
+      inFocus: false,
+    }
+  })
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,39 +74,58 @@ const SignUpForm = props => {
   }, [])
 
   useEffect(() => {
-    validate();
+    let isFormValid = true;
+    Object.keys(form).forEach(key => {
+      if (!form[key].isValid) {
+        isFormValid = false
+      }
+    })
+    setIsFormValid(isFormValid);
   }, [form])
 
-  const validate = async () => {
-    // check if form is valid, if yes, buttons is gonna stop being disabled
-    if (
-      form.name.length < 2 || form.name.length > 60 ||
-      !/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(form.email) ||
-      form.phone.indexOf('+380') != 0 ||
-      form.photo === '' || !form.photo || form.phone === '' ||
-      form.photo.size >= (5 * 1000000)
-    ) {
-      setIsValid(false);
+  const changeHandler = (value, fieldName) => {
+    const newForm = { ...form };
+    newForm[fieldName] = {
+      ...form[fieldName],
+      value,
+      isValid: form[fieldName].validate(value),
+      triedInput: true
     }
-    else {
-      setIsValid(true);
-    }
+    setForm(newForm);
   }
 
-  const submitHandler = async event => {
+  const focusHandler = (focused, fieldName) => {
+    const newForm = { ...form };
 
+    newForm[fieldName] = {
+      ...form[fieldName],
+      inFocus: focused
+    }
+    setForm(newForm);
+  }
+
+
+  const submitHandler = async event => {
     // create formdata and send it, after refresh the user cards
     event.preventDefault();
     const formData = new FormData();
 
-    formData.append('position_id', form.position_id);
-    formData.append('name', form.name);
-    formData.append('email', form.email);
-    formData.append('phone', form.phone);
-    formData.append('photo', form.photo);
+    Object.keys(form).forEach(key => {
+      const value = 'preSend' in form[key] ? form[key].preSend(form[key].value) : form[key].value;
+      formData.append(key, value);
+    });
 
     await addUser(formData);
     await props.getUsers(0);
+
+    // reset form
+    const newForm = { ...form };
+    Object.keys(form).forEach(key => {
+      newForm[key] = {
+        ...form[key], value: '', isValid: false,
+        triedInput: false,
+      }
+    });
   }
 
   if (!positions) {
@@ -71,9 +136,27 @@ const SignUpForm = props => {
     <div className="sign-up-form" >
       <h2 className="title h-center-container margin-title" id="signup">Working with POST request</h2>
       <form method="POST" onSubmit={submitHandler}>
-        <input className="input-text" placeholder="Your name" name="name" value={form.name} onChange={event => setForm({ ...form, name: event.target.value })}></input>
-        <input className="input-text" placeholder="Email" name="email" type="email" value={form.email} onChange={event => setForm({ ...form, email: event.target.value })}></input>
-        <input className="input-text" placeholder="Phone" name="phone" value={form.phone} onChange={event => setForm({ ...form, phone: event.target.value })}></input>
+        {
+          Object.keys(form).map(field => {
+            if (form[field].render !== false) {
+              const obj = form[field];
+
+              return (
+                <div key={field} >
+                  <input
+                    onFocus={() => focusHandler(true, field)}
+                    onBlur={() => focusHandler(false, field)}
+                    className={`input-text ${!obj.isValid && obj.triedInput ? 'input-error' : ''}`}
+                    placeholder={obj.placeholder}
+                    name={field}
+                    value={obj.value} onChange={(event) => changeHandler(event.target.value, field)}></input>
+
+                  <label className={`label-error ${field === 'phone' ? 'label-phone' : ''}`} style={!obj.isValid && obj.triedInput && !obj.inFocus ? { display: 'block' } : { display: 'none' }}>{obj.errorMessage}</label>
+                </div>
+              )
+            }
+          })
+        }
         <label className="phone-mask-label" htmlFor="phone"  >+38 (XXX) XXX-XX-XX</label>
 
         <label className="position-label" >Select your position</label>
@@ -81,20 +164,22 @@ const SignUpForm = props => {
           positions.map(p => {
             return (
               <label key={p.id} className="radio-container">{p.name}
-                <input type="radio" name="position" checked={p.id == form.position_id ? true : false} onChange={event => setForm({ ...form, position_id: p.id })} />
+                <input type="radio" name="position" checked={p.id == form.position_id.value ? true : false} onChange={(event) => changeHandler(p.id, 'position_id')} />
                 <span className="checkmark"></span>
               </label>
             );
           })
         }
+
         <label className="input-container-file">
           <input className="input-file" type="file" name="photo" accept="image/jpg, image/jpeg"
-            onChange={event => { setForm({ ...form, photo: event.target.files[0] }) }} />
+            onChange={(event) => changeHandler(event.target.files[0], 'photo')} />
           <div className="btn-upload" type="file"><p>Upload</p></div>
-          <div className="upload-text" onClick={event => event.preventDefault()}><p>{form.photo === '' ? 'Upload your photo' : form.photo.name}</p></div>
+          <div className="upload-text" onClick={event => event.preventDefault()}><p>{form.photo.value === '' ? 'Upload your photo' : form.photo.value.name}</p></div>
         </label>
+        <label className="label-error" style={!form.photo.isValid && form.photo.triedInput ? { display: 'block' } : { display: 'none' }}>{form.photo.errroMessage}</label>
 
-        <button className="btn btn-primary" disabled={!isValid}>Sign up</button>
+        <button className="btn btn-primary" disabled={!isFormValid}>Sign up</button>
 
       </form >
     </div >
